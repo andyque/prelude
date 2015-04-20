d7f6f77bc060042d5c6a8672f681409f2acf0bce
andyque <guanghui8827@126.com>
2015-04-20 22:53:23 +0800

    use ivy to relace ido and add melpa-stable

--- personal/personal-completion.el
+++ personal/personal-completion.el
@@ -48,4 +48,28 @@
 ;; (ac-set-trigger-key "<tab>")
 
 
+;; http://oremacs.com/2015/04/16/ivy-mode/
+(require 'ivy)
+(ido-mode -1)
+(ivy-mode 1)
+(setq magit-completing-read-function 'ivy-completing-read)
+
+
+;; (prelude-require-packages '(ido-vertical-mode))
+;; (ido-vertical-mode t)
+
+;; ;;face config like abo-abo
+;; (setq ido-vertical-show-count t)
+;; (setq ido-use-faces t)
+;; (set-face-attribute 'ido-vertical-first-match-face nil
+;;                     :background "#e5b7c0")
+;; (set-face-attribute 'ido-vertical-only-match-face nil
+;;                     :background "#e52b50"
+;;                     :foreground "white")
+;; (set-face-attribute 'ido-vertical-match-face nil
+;;                     :foreground "#b00000")
+;; (ido-vertical-mode 1)
+
+;; Now I use ivy instead
+
 (provide 'personal-completion)
