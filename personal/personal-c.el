cbd532bc0a025652b05f3161d11653dc1a0e94c6
andyque <guanghui8827@126.com>
2015-04-23 15:42:02 +0800

    add google-c style

--- personal/personal-c.el
+++ personal/personal-c.el
@@ -5,103 +5,107 @@
                             stickyfunc-enhance
                             company-irony
                             cmake-font-lock
+                            stickyfunc-enhance
                             company-c-headers
-                            ;; srefactor
-                            ))
+                            srefactor
+                            google-c-style
+                            function-args))
 
 (use-package flycheck-irony)
 
 (setq use-package-verbose t)
 (defun c-c++/init-cc-mode()
   (use-package c++-mode
-               :init
-               (progn
-                 (require 'semantic/bovine/c)
-                 (require 'semantic/ia)
-                 (setq semanticdb-default-save-directory
-                       (expand-file-name "semanticdb" prelude-savefile-dir))
+    :init
+    (progn
+      (require 'semantic/bovine/c)
+      (require 'semantic/ia)
 
-                 (add-to-list 'semantic-default-submodes 'global-semanticdb-minor-mode)
-                 (add-to-list 'semantic-default-submodes 'global-semantic-mru-bookmark-mode)
-                 (add-to-list 'semantic-default-submodes 'global-semantic-idle-scheduler-mode)
-                 (add-to-list 'semantic-default-submodes 'global-semantic-idle-summary-mode)
-                 (add-to-list 'semantic-default-submodes 'global-semantic-idle-local-symbol-highlight-mode)
-                 (add-to-list 'semantic-default-submodes 'global-semantic-stickyfunc-mode)
-                 (add-to-list 'semantic-default-submodes 'global-semantic-highlight-func-mode)
-                 ;; (add-to-list 'semantic-default-submodes 'global-semantic-decoration-mode)
+      (add-to-list 'semantic-default-submodes 'global-semanticdb-minor-mode)
+      (add-to-list 'semantic-default-submodes 'global-semantic-mru-bookmark-mode)
+      (add-to-list 'semantic-default-submodes 'global-semantic-idle-scheduler-mode)
+      (add-to-list 'semantic-default-submodes 'global-semantic-idle-summary-mode)
+      (add-to-list 'semantic-default-submodes 'global-semantic-idle-local-symbol-highlight-mode)
+      (add-to-list 'semantic-default-submodes 'global-semantic-stickyfunc-mode)
+      (add-to-list 'semantic-default-submodes 'global-semantic-highlight-func-mode)
+      ;; (add-to-list 'semantic-default-submodes 'global-semantic-decoration-mode)
 
-                 ;; The following settings sometimes is very annoying, flycheck-irony is more sutiable
-                 ;; (add-to-list 'semantic-default-submodes 'global-semantic-show-unmatched-syntax-mode)
-                 (add-to-list 'semantic-default-submodes 'global-semantic-show-parser-state-mode)
-                 (add-to-list 'semantic-default-submodes 'global-semantic-highlight-edits-mode)
+      ;; The following settings sometimes is very annoying, flycheck-irony is more sutiable
+      ;; (add-to-list 'semantic-default-submodes 'global-semantic-show-unmatched-syntax-mode)
+      (add-to-list 'semantic-default-submodes 'global-semantic-show-parser-state-mode)
+      (add-to-list 'semantic-default-submodes 'global-semantic-highlight-edits-mode)
 
-                 (setq c-default-style "linux") ;; set style to "linux"
-                 (setq c-basic-offset 4)
-                 (c-set-offset 'substatement-open 0)
-                 (setq cc-other-file-alist
-                       '(("\\.cpp"   (".h"))
-                         ("\\.h"   (".c"".cpp"))))
+      (semantic-mode t)
 
-                 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-                 (add-to-list 'auto-mode-alist '("\\.c\\'" . c++-mode))
-                 (add-to-list 'auto-mode-alist '("\\.mm\\'" . objc-mode))
-                 (add-hook 'c-mode-common-hook 'yas-minor-mode)
-                 (add-hook 'c-mode-common-hook 'semantic-mode)
-                 (defvar cocos2dx-dir "~/cocos2d-x")
-                 (semantic-add-system-include cocos2dx-dir 'c++-mode)
-                 (semantic-add-system-include (concat cocos2dx-dir "/cocos") 'c++-mode)
-                 (semantic-add-system-include (concat cocos2dx-dir "/platform") 'c++-mode)
-                 (semantic-add-system-include (concat cocos2dx-dir "/audio/include") 'c++-mode)
-                 (semantic-add-system-include (concat cocos2dx-dir "/platform/mac") 'c++-mode)
-                 (semantic-add-system-include (concat cocos2dx-dir "/extensions") 'c++-mode)
-                 (semantic-add-system-include (concat cocos2dx-dir "/external/mac/x86-64/include") 'c++-mode)
-                 (semantic-add-system-include (concat cocos2dx-dir "/external/mac/x86-64/include/luajit") 'c++-mode)
-                 (semantic-add-system-include (concat cocos2dx-dir "/external/mac/x86-64/include/freetype") 'c++-mode)
-                 (semantic-add-system-include (concat cocos2dx-dir "/external/mac/x86-64/include/zlib") 'c++-mode)
-                 ;;include path for OpenGL
-                 (semantic-add-system-include "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk/System/Library/Frameworks/OpenGL.framework/Versions/A/Headers" 'c++-mode)
-                 (add-to-list 'auto-mode-alist (cons cocos2dx-dir 'c++-mode))
-                 (add-to-list 'semantic-lex-c-preprocessor-symbol-map '("CC_DLL" . ""))
-                 (add-to-list 'semantic-lex-c-preprocessor-symbol-map '("EXPORT_DLL" . ""))
-                 (add-to-list 'semantic-lex-c-preprocessor-symbol-map '("_USRDLL" . ""))
-                 (add-to-list 'semantic-lex-c-preprocessor-symbol-map '("CC_TARGET_OS_MAC" . "1"))
-                 (add-to-list 'semantic-lex-c-preprocessor-symbol-map '("_USRDLL" . ""))
-                 (add-to-list 'semantic-lex-c-preprocessor-symbol-map '("CC_GUI_DLL" . ""))
-                 (add-to-list 'semantic-lex-c-preprocessor-symbol-map '("CC_KEYBOARD_SUPPORT" . "1"))
-                 (add-to-list 'semantic-lex-c-preprocessor-symbol-map '("CC_DEPRECATED_ATTRIBUTE" . ""))
-                 (add-to-list 'semantic-lex-c-preprocessor-symbol-map '("USE_FILE32API" . "1"))
-                 (add-to-list 'semantic-lex-c-preprocessor-symbol-map '("CC_ENABLE_CHIPMUNK_INTEGRATION" . "1"))
-                 (add-to-list 'semantic-lex-c-preprocessor-symbol-map '("COCOS2D_DEBUG" . "1"))
-                 (add-to-list 'semantic-lex-c-preprocessor-symbol-file (concat cocos2dx-dir "/cocos/platform/mac/CCPlatformDefine-mac.h"))
-                 (add-to-list 'semantic-lex-c-preprocessor-symbol-file (concat cocos2dx-dir "/cocos/platform/CCPlatformMacros.h"))
+      ;; use google-c-style instead
+      ;; (setq c-default-style "linux") ;; set style to "linux"
+      ;; (setq c-basic-offset 4)
+      ;; (c-set-offset 'substatement-open 0)
+      (setq cc-other-file-alist
+            '(("\\.cpp"   (".h"))
+              ("\\.h"   (".c"".cpp"))))
 
-                 (set-default 'semantic-case-fold t)
-                 )
-               :config
-               (progn
-                 (diff-hl-mode -1)
+      (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+      (add-to-list 'auto-mode-alist '("\\.c\\'" . c++-mode))
+      (add-to-list 'auto-mode-alist '("\\.mm\\'" . objc-mode))
+      
+      (add-hook 'c-mode-common-hook 'yas-minor-mode)
+      (add-hook 'c-mode-common-hook 'google-set-c-style)
+      
+      (defvar cocos2dx-dir "~/cocos2d-x")
+      (semantic-add-system-include cocos2dx-dir 'c++-mode)
+      (semantic-add-system-include (concat cocos2dx-dir "/cocos") 'c++-mode)
+      (semantic-add-system-include (concat cocos2dx-dir "/platform") 'c++-mode)
+      (semantic-add-system-include (concat cocos2dx-dir "/audio/include") 'c++-mode)
+      (semantic-add-system-include (concat cocos2dx-dir "/platform/mac") 'c++-mode)
+      (semantic-add-system-include (concat cocos2dx-dir "/extensions") 'c++-mode)
+      (semantic-add-system-include (concat cocos2dx-dir "/external/mac/x86-64/include") 'c++-mode)
+      (semantic-add-system-include (concat cocos2dx-dir "/external/mac/x86-64/include/luajit") 'c++-mode)
+      (semantic-add-system-include (concat cocos2dx-dir "/external/mac/x86-64/include/freetype") 'c++-mode)
+      (semantic-add-system-include (concat cocos2dx-dir "/external/mac/x86-64/include/zlib") 'c++-mode)
+      ;;include path for OpenGL
+      (semantic-add-system-include "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk/System/Library/Frameworks/OpenGL.framework/Versions/A/Headers" 'c++-mode)
+      (add-to-list 'auto-mode-alist (cons cocos2dx-dir 'c++-mode))
+      (add-to-list 'semantic-lex-c-preprocessor-symbol-map '("CC_DLL" . ""))
+      (add-to-list 'semantic-lex-c-preprocessor-symbol-map '("EXPORT_DLL" . ""))
+      (add-to-list 'semantic-lex-c-preprocessor-symbol-map '("_USRDLL" . ""))
+      (add-to-list 'semantic-lex-c-preprocessor-symbol-map '("CC_TARGET_OS_MAC" . "1"))
+      (add-to-list 'semantic-lex-c-preprocessor-symbol-map '("_USRDLL" . ""))
+      (add-to-list 'semantic-lex-c-preprocessor-symbol-map '("CC_GUI_DLL" . ""))
+      (add-to-list 'semantic-lex-c-preprocessor-symbol-map '("CC_KEYBOARD_SUPPORT" . "1"))
+      (add-to-list 'semantic-lex-c-preprocessor-symbol-map '("CC_DEPRECATED_ATTRIBUTE" . ""))
+      (add-to-list 'semantic-lex-c-preprocessor-symbol-map '("USE_FILE32API" . "1"))
+      (add-to-list 'semantic-lex-c-preprocessor-symbol-map '("CC_ENABLE_CHIPMUNK_INTEGRATION" . "1"))
+      (add-to-list 'semantic-lex-c-preprocessor-symbol-map '("COCOS2D_DEBUG" . "1"))
+      (add-to-list 'semantic-lex-c-preprocessor-symbol-file (concat cocos2dx-dir "/cocos/platform/mac/CCPlatformDefine-mac.h"))
+      (add-to-list 'semantic-lex-c-preprocessor-symbol-file (concat cocos2dx-dir "/cocos/platform/CCPlatformMacros.h"))
 
-                 ;; configs for semantic db
-                 ;; https://martinralbrecht.wordpress.com/2014/11/03/c-development-with-emacs/
-                 ;; (require 'srefactor)
-                 ;; (define-key c-mode-map (kbd "M-RET") 'srefactor-refactor-at-point)
-                 ;; (define-key c++-mode-map (kbd "M-RET") 'srefactor-refactor-at-point)
-                 ;; (add-hook 'srefactor-ui-menu-mode-hook (lambda()(smartparens-mode -1)))
-                 ;; ;;modify setters and getters style
-                 ;; (setq srefactor--getter-prefix "get")
-                 ;; (setq srefactor--setter-prefix "set")
+      (set-default 'semantic-case-fold t))
+    :config
+    (progn
+      (diff-hl-mode -1)
 
-
-                 ;; (require 'ede)
-                 ;; (global-ede-mode)
-
-                 ;; enhancement for sticky mode
-                 (require 'stickyfunc-enhance)
+      ;; configs for semantic db
+      ;; https://martinralbrecht.wordpress.com/2014/11/03/c-development-with-emacs/
+      (require 'srefactor)
+      (define-key c-mode-map (kbd "M-RET") 'srefactor-refactor-at-point)
+      (define-key c++-mode-map (kbd "M-RET") 'srefactor-refactor-at-point)
+      (add-hook 'srefactor-ui-menu-mode-hook (lambda()(smartparens-mode -1)))
+      ;;modify setters and getters style
+      (setq srefactor--getter-prefix "get")
+      (setq srefactor--setter-prefix "set")
 
 
-                 (require 'function-args)
-                 (fa-config-default)
-                 )))
+      ;; (require 'ede)
+      ;; (global-ede-mode)
+
+      ;; enhancement for sticky mode
+      (require 'stickyfunc-enhance)
+
+
+      (require 'function-args)
+      (fa-config-default)
+      )))
 (c-c++/init-cc-mode)
 
 
